b4581c68f4a64d7690173869c7878f5eb009de86
Manuel Duarte <carlosd99m@gmail.com>
2021-12-03 00:36:31 -0600

    add api key

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-
-
 import androidx.compose.desktop.Window
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.unit.IntSize
 
-const val API_KEY = "9f1ad3d2cda545c281264554210212"
+const val API_KEY = ""
 
 fun main() = Window(
-  title = "Sunny Desk",
-  size = IntSize(800, 700),
+    title = "Sunny Desk (KT)",
+    size = IntSize(800, 700),
 ) {
-  val repository = Repository(API_KEY)
+    val repository = Repository(API_KEY)
 
-  MaterialTheme {
-    WeatherScreen(repository)
-  }
+    MaterialTheme {
+        WeatherScreen(repository)
+    }
 }
--- src/main/kotlin/Repository.kt
+++ src/main/kotlin/Repository.kt
@@ -1,3 +1,4 @@
+
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
@@ -5,28 +6,41 @@ import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 
 class Repository(
-    private val apiKey: String,
-    private val client: HttpClient = defaultHttpClient,
+  private val apiKey: String,
+  private val client: HttpClient = defaultHttpClient,
 ) {
-    suspend fun getWeatherForCity(city: String): WeatherResponse =
-        client.get(
-            "https://api.weatherapi.com/v1/forecast.json" +
-                    "?key=$apiKey" +
-                    "&q=$city" +
-                    "&days=5" +
-                    "&aqi=no" +
-                    "&alerts=no"
-        )
+  private val transformer = WeatherTransformer()
 
-    companion object {
-        val defaultHttpClient = HttpClient(CIO) {
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(
-                    json = kotlinx.serialization.json.Json {
-                        ignoreUnknownKeys = true
-                    }
-                )
-            }
-        }
+  private suspend fun getWeatherForCity(city: String): WeatherResponse =
+    client.get(
+      "https://api.weatherapi.com/v1/forecast.json" +
+          "?key=$apiKey" +
+          "&q=$city" +
+          "&days=5" +
+          "&aqi=no" +
+          "&alerts=no"
+    )
+
+  suspend fun weatherForCity(city: String): Lce<WeatherResults> {
+    return try {
+      val result = getWeatherForCity(city)
+      val content = transformer.transform(result)
+      Lce.Content(content)
+    } catch (e: Exception) {
+      e.printStackTrace()
+      Lce.Error(e)
     }
+  }
+
+  companion object {
+    val defaultHttpClient = HttpClient(CIO) {
+      install(JsonFeature) {
+        serializer = KotlinxSerializer(
+          json = kotlinx.serialization.json.Json {
+            ignoreUnknownKeys = true
+          }
+        )
+      }
+    }
+  }
 }
